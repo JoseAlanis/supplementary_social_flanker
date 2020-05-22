24f807d91d22a733e2d1fe67240dc7dac1f5167d
Jose C. Garcia Alanis <12409129+JoseAlanis@users.noreply.github.com>
2020-05-22 12:58:53 +0200

    add subj 13 to config

--- config.py
+++ config.py
@@ -80,9 +80,9 @@ montage = make_standard_montage(kind='standard_1020')
 exclude = ['EXG4', 'EXG5', 'EXG6', 'EXG7', 'EXG8']
 
 # subjects to use for analysis
-subjects = [2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 14, 15, 16, 17, 18,
+subjects = [2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
             19, 20, 23, 24, 27, 28, 29, 31, 32, 33, 34, 35,
-            36, 38, 37]
+            36, 37, 38]
 
 # relevant events in the paradigm
 event_ids = {'flanker_onset': 71,
